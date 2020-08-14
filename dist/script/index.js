f3afdf209e88c77b66bfe5379156fb6b73ca5ef2
SanRenYanBlog <sanrenyan@gmail.com>
2020-08-14 15:22:07 +0800

    index.html commit

--- dist/script/index.js
+++ dist/script/index.js
@@ -3,7 +3,6 @@ let bantoLeft = document.querySelector('.bantoLeft');
 let bantoRight = document.querySelector('.bantoRight');
 let banDotts = document.querySelectorAll('.banDotts span');
 let banImgs = document.querySelectorAll('.imgAll div');
-console.log(banImgs);
 let timer = null;
 let nowIndex = 0;
 banDotts[nowIndex].className = 'checked';
@@ -17,6 +16,42 @@ function banMove(){
         nowIndex = nowIndex > 2 ? 0 : nowIndex++;
         banDotts[nowIndex].className = 'checked';
         banImgs[nowIndex].style.opacity = '1';
-    },3000)
+    },2000)
+}
+
+bantoLeft.addEventListener('click',clickHandler);
+bantoRight.addEventListener('click',clickHandler);
+for(let i=0;i<banDotts.length;i++){
+    banDotts[i].addEventListener('click',clickHandler);
+}
+function clickHandler(e){
+    clearInterval(timer);
+    console.log(nowIndex);
+    banDotts[nowIndex].className = '';
+    banImgs[nowIndex].style.opacity = '0';
+    if(e.target.className === 'bantoLeft'){
+        nowIndex--;
+        nowIndex = nowIndex < 0 ? banImgs.length-1 : nowIndex--;
+        banDotts[nowIndex].className = 'checked';
+        banImgs[nowIndex].style.opacity = '1';
+    }
+    if(e.target.className === 'bantoRight'){
+        nowIndex++;
+        nowIndex = nowIndex > banImgs.length-1 ? 0 : nowIndex++;
+        banDotts[nowIndex].className = 'checked';
+        banImgs[nowIndex].style.opacity = '1';
+    }
+    if(e.target.parentNode.className === 'banDotts'){
+        for(let i=0;i<banDotts.length;i++){
+            if(banDotts[i] === e.target){
+                banDotts[nowIndex].className = '';
+                banImgs[nowIndex].style.opacity = '0';
+                nowIndex = i;
+                banDotts[nowIndex].className = 'checked';
+                banImgs[nowIndex].style.opacity = '1';
+            }
+        }
+    }
+    banMove();
 }
 
